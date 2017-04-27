c28db72131b98c25c22307710f1024df3edc81cf
Rovanion Luckey <rovanion.luckey@gmail.com>
2017-04-27 15:58:47 +0200

    Upped version of strgen to fix character group bug.

--- project.clj
+++ project.clj
@@ -18,7 +18,7 @@
                  [clojure-complete "0.2.4"]
                  [prismatic/schema "1.1.5"]
                  [prismatic/schema-generators "0.1.0"]
-                 [com.velisco/strgen "0.1.2"]
+                 [com.velisco/strgen "0.1.3"]
                  ;; bump versions of various common transitive deps
                  [scout "0.1.1"]
                  [slingshot "0.12.2"]
